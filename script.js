9b4d9a035ea85a6618693d88fd085823bb5ccba1
Jayson576 <72175843+Jayson576@users.noreply.github.com>
2020-12-21 23:32:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
+$(document).ready(function () {
 let playlist = [
-	{src:'https://www.bensound.com//bensound-music/bensound-letitgo.mp3', title:"Letitgo", artist:"stereochipz"},
-	{src:'https://www.bensound.com//bensound-music/bensound-thejazzpiano.mp3', title:"JazzPiano", artist:"Benjamin Tissot"},
-  {src:'https://www.bensound.com//bensound-music/bensound-whenyousmile.mp3', title:"When You Smile", artist:"DpMusic"},
-  {src:'https://www.bensound.com//bensound-music/bensound-vista.mp3', title:"Vista", artist:"stereochipz"},
-  {src:'https://www.bensound.com//bensound-music/bensound-thisweremember.mp3', title:"This We Remember", artist:"Dan Phillipson"},
-  {src:'https://www.bensound.com//bensound-music/bensound-resurgence.mp3', title:"Resurgence", artist:"Anton Vlasov"},
-  {src:'https://www.bensound.com//bensound-music/bensound-showoff.mp3', title:"Show off", artist:"Anton Vlasov"},
-  {src:'https://www.bensound.com//bensound-music/bensound-wakemeup.mp3', title:"wake me up", artist:"Anton Vlasov"},
-  {src:'https://www.bensound.com//bensound-music/bensound-hipjazz.mp3', title:"Hip Jazz", artist:"Benjamin Tissot"},
-  {src:'https://www.bensound.com//bensound-music/bensound-sunday.mp3', title:"Sunday", artist:"Anton Vlasov"},
+	{src:'https://www.bensound.com//bensound-music/bensound-letitgo.mp3', title:"Let It Go By", artist:"Stereochipz"},
+	{src:'https://www.bensound.com//bensound-music/bensound-thejazzpiano.mp3', title:"JazzPiano By", artist:"Benjamin Tissot"},
+  {src:'https://www.bensound.com//bensound-music/bensound-whenyousmile.mp3', title:"When You Smile By", artist:"DpMusic"},
+  {src:'https://www.bensound.com//bensound-music/bensound-vista.mp3', title:"Vista By", artist:"stereochipz"},
+  {src:'https://www.bensound.com//bensound-music/bensound-thisweremember.mp3', title:"This We Remember By", artist:"Dan Phillipson"},
+  {src:'https://www.bensound.com//bensound-music/bensound-resurgence.mp3', title:"Resurgence By", artist:"Anton Vlasov"},
+  {src:'https://www.bensound.com//bensound-music/bensound-showoff.mp3', title:"Show Off By", artist:"Anton Vlasov"},
+  {src:'https://www.bensound.com//bensound-music/bensound-wakemeup.mp3', title:"Wake Me Up By", artist:"Anton Vlasov"},
+  {src:'https://www.bensound.com//bensound-music/bensound-hipjazz.mp3', title:"Hip Jazz By", artist:"Benjamin Tissot"},
+  {src:'https://www.bensound.com//bensound-music/bensound-sunday.mp3', title:"Sunday By", artist:"Anton Vlasov"},
   
 ]
 let whichSongAmIOn = 0
 let isPlaying = false
 let audio = null
+
+artistname = $(".music-artist");
+titlename = $(".music-title");
+  
 let playASong = function(whichSong) {
 	let song = playlist[whichSong] 
 	if (audio) { 
@@ -25,14 +30,22 @@ let playASong = function(whichSong) {
 }
 
 let pressedPlay = function() {
+   
 	if (isPlaying) {
+ 
 		audio.pause()
 		isPlaying = false
 		play.textContent = `▶play`
+    artistname.html(playlist[whichSongAmIOn].artist) 
+titlename.html(playlist[whichSongAmIOn].title)
 	} else {
+     
+    
 		audio.play()
 		isPlaying = true
 		play.textContent = `Pause`
+   artistname.html(playlist[whichSongAmIOn].artist)
+    titlename.html(playlist[whichSongAmIOn].title)
 	}
 }
 
@@ -42,18 +55,28 @@ let play = document.querySelector(`#play`)
 play.addEventListener(`click`, pressedPlay)
 
 let pressedNext = function() {
+ 
+  
 	whichSongAmIOn = whichSongAmIOn + 1
 	playASong(whichSongAmIOn)
+   artistname.html(playlist[whichSongAmIOn].artist) 
+titlename.html(playlist[whichSongAmIOn].title)
 }
 
 let next = document.querySelector(`#next`)
 next.addEventListener(`click`, pressedNext)
 
 let pressedBack = function() {
+   
 	whichSongAmIOn = whichSongAmIOn - 1
 	playASong(whichSongAmIOn)
+  artistname.html(playlist[whichSongAmIOn].artist) 
+titlename.html(playlist[whichSongAmIOn].title)
 }
 let back = document.querySelector(`#back`)
 back.addEventListener(`click`, pressedBack)
 
 playASong(0)
+
+
+});
